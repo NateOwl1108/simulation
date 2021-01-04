3f60639f793d8b94756e6866361654896eb61cfd
NateOwl <natedog112004@gmail.com>
2021-01-04 03:54:24 +0000

    I modified the euler estimator class to take in a dict of derivatives and and a dict of points.

--- src/euler_estimator.py
+++ src/euler_estimator.py
@@ -1,36 +1,54 @@
 class EulerEstimator():
-  def __init__(self, derivative):
-    self.derivative = derivative
+  def __init__(self, derivatives):
+    self.derivatives = derivatives
   
   def calc_derivative_at_point(self, point):
-    coordinates = str(point)
-    coordinates = coordinates.replace('(', '')
-    coordinates = coordinates.replace(')', '')
-    coordinates = coordinates.split(',')
-
-    x_value = float(coordinates[0])
-    return self.derivative(x_value)
+    point_list = []
+    for char in point:
+      if char != '(' or char != ')':
+        point_list.append(char)
+    x = point_list[0]
+    values = point_list[1]
+    return_values = dict(values)
+    
+    for key in values:
+      return_values[key] =  self.derivatives[key](x, values)
+    return return_values
 
   def step_forward(self, point, step_size):
-    coordinates = str(point)
-    coordinates = coordinates.replace('(', '')
-    coordinates = coordinates.replace(')', '')
-    coordinates = coordinates.split(',')
     
-    x_value = float(coordinates[0])
-    y_value = float(coordinates[1])
-    y_value += step_size * self.calc_derivative_at_point(point)
-    y_value = round(y_value, 2)
-    x_value += step_size
-    x_value = round(x_value, 2)
-    return(x_value,y_value)
+    point_list = []
+    for char in point:
+      if char != '(' or char != ')':
+        point_list.append(char)
+    x = point_list[0] + step_size
+    values = point_list[1]
+    derivative = self.calc_derivative_at_point(point)
+    for key in values:
+      values[key] = round(values[key] + step_size * derivative[key],9)
+  
+    return(x, values)
   
   def calc_estimated_points(self, point , step_size, num_steps):
+    point_list = []
+    for char in point:
+      if char != '(' or char != ')':
+        point_list.append(char)
+    x = point_list[0]
+    value_dict = dict(point_list[1])
+    point_x = float(x)
     values = []
-    values.append(point)
+    values.append((point_x,value_dict))
     for i in range(num_steps):
       point = self.step_forward(point, step_size)
-      values.append(point)
+      point_list = []
+      for char in point:
+        if char != '(' or char != ')':
+          point_list.append(char)
+      x = point_list[0]
+      value_dict = dict(point_list[1])
+      point_x = float(x)
+      values.append((point_x,value_dict))
       
     return values
 
--- tests/test_euler_estimator.py
+++ tests/test_euler_estimator.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('src')
 from euler_estimator import EulerEstimator
-
+'''
 
 euler = EulerEstimator(derivative = (lambda t: t+1))
 
@@ -15,4 +15,32 @@ assert euler.calc_estimated_points(point=(1,4), step_size=0.5, num_steps=4) == [
     (2, 6.25), # after 2nd step
     (2.5, 7.75), # after 3rd step
     (3, 9.5) # after 4th step
+]'''
+
+derivatives = {
+        'A': (lambda t,x: x['A'] + 1),
+        'B': (lambda t,x: x['A'] + x['B']),
+        'C': (lambda t,x: 2*x['B']) 
+    }
+
+euler = EulerEstimator(derivatives = derivatives)
+
+initial_values = {'A': 0, 'B': 0, 'C': 0}
+initial_point = (0, initial_values)
+
+assert euler.calc_derivative_at_point(initial_point) == {'A': 1, 'B': 0, 'C': 0}
+
+point_2 = euler.step_forward(point = initial_point, step_size = 0.1)
+assert point_2 == (0.1, {'A': 0.1, 'B': 0, 'C': 0})
+
+assert euler.calc_derivative_at_point(point_2) == {'A': 1.1, 'B': 0.1, 'C': 0}
+
+point_3 = euler.step_forward(point = point_2, step_size = -0.5)
+assert point_3 == (-0.4, {'A': -0.45, 'B': -0.05, 'C': 0})
+
+assert euler.calc_estimated_points(point=point_3, step_size=2, num_steps=3) == [
+   (-0.4, {'A': -0.45, 'B': -0.05, 'C': 0}), # starting point 
+   (1.6, {'A': 0.65, 'B': -1.05, 'C': -0.2}), # after 1st step
+   (3.6, {'A': 3.95, 'B': -1.85, 'C': -4.4}), # after 2nd step
+   (5.6, {'A': 13.85, 'B': 2.35, 'C': -11.8}) # after 3rd step
 ]
