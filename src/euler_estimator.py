175e727573dc4066673ab9afbada845a4e9827aa
NateOwl <natedog112004@gmail.com>
2021-01-04 01:43:16 +0000

    created the class EulerEstimator which takes a derivitive. It has the functions calc_derivative_at_point, step_foward which estimates the next value given a increase and calc_estimated_points for step_size and num_steps.

--- src/euler_estimator.py
+++ src/euler_estimator.py
@@ -1,66 +1,42 @@
 class EulerEstimator():
-  def __init__(self, derivative, point):
+  def __init__(self, derivative):
+    self.derivative = derivative
+  
+  def calc_derivative_at_point(self, point):
+    coordinates = str(point)
+    coordinates = coordinates.replace('(', '')
+    coordinates = coordinates.replace(')', '')
+    coordinates = coordinates.split(',')
+
+    x_value = float(coordinates[0])
+    return self.derivative(x_value)
+
+  def step_forward(self, point, step_size):
     coordinates = str(point)
     coordinates = coordinates.replace('(', '')
     coordinates = coordinates.replace(')', '')
     coordinates = coordinates.split(',')
     
-    self.x_value = int(coordinates[0])
-    self.y_value = int(coordinates[1])
-    self.point = (self.x_value, self.y_value)
-    self.derivative = derivative
+    x_value = float(coordinates[0])
+    y_value = float(coordinates[1])
+    y_value += step_size * self.calc_derivative_at_point(point)
+    y_value = round(y_value, 2)
+    x_value += step_size
+    x_value = round(x_value, 2)
+    return(x_value,y_value)
   
-  def calc_derivative_at_point(self):
-    return self.derivative(self.x_value)
-
-  def step_forward(self, step):
-    self.y_value += step * self.calc_derivative_at_point()
-    self.y_value = round(self.y_value, 2)
-    self.x_value += step
-    self.x_value = round(self.x_value, 2)
-    self.point = (self.x_value, self.y_value)
-  
-  def calc_estimated_points(self, initial_point , step_size, num_steps):
-    coordinates = str(initial_point)
-    coordinates = coordinates.replace('(', '')
-    coordinates = coordinates.replace(')', '')
-    coordinates = coordinates.split(',')
-    self.x_value = float(coordinates[0])
-    self.y_value = float(coordinates[1])
+  def calc_estimated_points(self, point , step_size, num_steps):
+    values = []
+    values.append(point)
     for i in range(num_steps):
-      self.step_forward(step_size)
-      print(self.point)
+      point = self.step_forward(point, step_size)
+      values.append(point)
+      
+    return values
 
 
 
 
-euler = EulerEstimator(derivative = (lambda t: t+1),
-                           point = (1,4))
 
-print(euler.point)
-(1,4)
-
-print(euler.calc_derivative_at_point())
-2   
-
-euler.step_forward(0.1)
-print(euler.point)
-(1.1, 4.2)      
-
-print(euler.calc_derivative_at_point())
-2.1
-
-euler.step_forward(-0.5)
-print(euler.point)
-(0.6, 3.15)    
-
-euler.calc_estimated_points((0.6, 3.15),0.5,4)
-[
-    (0.6, 3.15),
-    (1.1, 3.95), # after 1st step
-    (1.6, 5), # after 2nd step
-    (2.1, 6.3), # after 3rd step
-    (2.6, 7.85), # after 4th step
-]
 
 
--- tests/test_euler_estimator.py
+++ tests/test_euler_estimator.py
@@ -3,31 +3,16 @@ sys.path.append('src')
 from euler_estimator import EulerEstimator
 
 
-euler = EulerEstimator(derivative = (lambda t: t+1),
-                           point = (1,4))
+euler = EulerEstimator(derivative = (lambda t: t+1))
 
-print(euler.point)
-(1,4)
+assert euler.calc_derivative_at_point((1,4)) == 2
 
-print(euler.calc_derivative_at_point())
-2   
+assert euler.step_forward(point=(1,4), step_size=0.5) == (1.5, 5)
 
-euler.step_forward(0.1)
-print(euler.point)
-(1.1, 4.2)      
-
-print(euler.calc_derivative_at_point())
-2.1
-
-euler.step_forward(-0.5)
-print(euler.point)
-(0.6, 3.15)    
-
-euler.calc_estimated_points((0.6, 3.15),0.5,4)
-[
-    (0.6, 3.15),
-    (1.1, 3.95), # after 1st step
-    (1.6, 5), # after 2nd step
-    (2.1, 6.3), # after 3rd step
-    (2.6, 7.85), # after 4th step
+assert euler.calc_estimated_points(point=(1,4), step_size=0.5, num_steps=4) == [
+    (1, 4), # starting point
+    (1.5, 5), # after 1st step
+    (2, 6.25), # after 2nd step
+    (2.5, 7.75), # after 3rd step
+    (3, 9.5) # after 4th step
 ]
